6f0c80b8a0d1950a9cbaf8f097b806177e7db9a4
Roderick Lan <rodericklan03@gmail.com>
2025-01-02 16:16:41 -0700

    cmake and xtensor, xtl

---
+++ build/CMakeFiles/Dory.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Dory.dir/src/main.cpp.o"
+  "CMakeFiles/Dory.dir/src/main.cpp.o.d"
+  "Dory"
+  "Dory.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Dory.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ src/main.cpp
@@ -0,0 +1,7 @@
+#include "xtensor/xbuilder.hpp"
+#include <xtensor.hpp>
+#include <iostream>
+
+int main() {
+    
+}
